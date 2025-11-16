ca1c4dd431879a6585a4774da142a3e07a12511f
sanixto <sanix.to@gmail.com>
2025-11-16 20:47:10 +0000

    add auth module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [AuthModule],
 })
 export class AppModule {}
