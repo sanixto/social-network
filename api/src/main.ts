4ca08d841aac7970d4fb9f95910c60362e9639f9
sanixto <sanix.to@gmail.com>
2025-11-25 07:45:36 +0000

    add helmet

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // strips properties not in DTO
